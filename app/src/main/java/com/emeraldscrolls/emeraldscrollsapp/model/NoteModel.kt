0694133df339fe75d77fd7f6f3ccd88141859d4f
gedeoaraujo <gedeao.lemobs@gmail.com>
2025-01-06 20:04:34 -0300

    Modificando o valor padrão da data.

--- app/src/main/java/com/emeraldscrolls/emeraldscrollsapp/model/NoteModel.kt
+++ app/src/main/java/com/emeraldscrolls/emeraldscrollsapp/model/NoteModel.kt
@@ -1,7 +1,9 @@
 package com.emeraldscrolls.emeraldscrollsapp.model
 
+import java.time.LocalDateTime
+
 data class NoteModel(
     var title: String = "",
     var text: String = "",
-    var date: String = "",
+    var date: LocalDateTime? = null,
 )
