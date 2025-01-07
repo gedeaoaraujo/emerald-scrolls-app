4d99032fe3ec53002391e4120aa77ea93e82f210
gedeoaraujo <gedeao.lemobs@gmail.com>
2025-01-07 14:04:02 -0300

    Adicionando componente de lista vazia.

--- app/src/main/java/com/emeraldscrolls/emeraldscrollsapp/home/RootHome.kt
+++ app/src/main/java/com/emeraldscrolls/emeraldscrollsapp/home/RootHome.kt
@@ -1,9 +1,11 @@
 package com.emeraldscrolls.emeraldscrollsapp.home
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -12,6 +14,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -23,10 +26,32 @@ import com.emeraldscrolls.emeraldscrollsapp.utils.dateTimeFmt
 
 @Composable
 fun RootHome(innerPadding: PaddingValues, notes: List<NoteModel>) {
-    LazyColumn(Modifier.padding(innerPadding)) {
-        items(notes){ item ->
-            NoteItem(item)
-            Spacer(Modifier.fillMaxWidth().background(Color.LightGray).size(1.dp))
+    Box(Modifier.padding(innerPadding)) {
+        if (notes.isEmpty()){
+            Column(
+                modifier = Modifier.fillMaxSize().padding(top = 64.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    fontSize = 24.sp,
+                    text = "The Library is empty",
+                    fontWeight = FontWeight.Bold,
+                    color = MaterialTheme.colorScheme.primary
+                )
+                Spacer(Modifier.size(16.dp))
+                Text(
+                    fontSize = 18.sp,
+                    text = "Write a new scroll to this library"
+                )
+            }
+        } else {
+            LazyColumn {
+                items(notes){ item ->
+                    NoteItem(item)
+                    Spacer(Modifier.fillMaxWidth()
+                        .background(Color.LightGray).size(1.dp))
+                }
+            }
         }
     }
 }
