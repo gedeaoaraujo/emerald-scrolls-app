f4c293cae9acb0c4ef5f497070716e1317158715
gedeoaraujo <gedeao.lemobs@gmail.com>
2025-01-07 17:54:48 -0300

    Extraindo o componente raiz pra um arquivo separado.

--- app/src/main/java/com/emeraldscrolls/emeraldscrollsapp/MainActivity.kt
+++ app/src/main/java/com/emeraldscrolls/emeraldscrollsapp/MainActivity.kt
@@ -4,47 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import com.emeraldscrolls.emeraldscrollsapp.home.RootHome
-import com.emeraldscrolls.emeraldscrollsapp.note.RootNote
-import com.emeraldscrolls.emeraldscrollsapp.ui.theme.EmeraldScrollsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent { RootApp() }
-    }
-}
-
-@Composable
-fun RootApp() {
-    val navController = rememberNavController()
-    val viewModel = MainViewModel()
-    val state by viewModel.state.collectAsState()
-
-    fun onCheckClick() {
-        navController.popBackStack()
-        viewModel.saveNewNote()
-    }
-
-    fun navigateTo(route: String){
-        navController.navigate(route)
-    }
-
-    EmeraldScrollsTheme {
-        NavHost(navController = navController, startDestination = "home") {
-            composable("home") {
-                RootHome(state.notes, ::navigateTo)
-            }
-            composable("note") {
-                RootNote(viewModel::onSaveNote, ::onCheckClick)
-            }
-        }
     }
 }
---
+++ app/src/main/java/com/emeraldscrolls/emeraldscrollsapp/RootApp.kt
@@ -0,0 +1,38 @@
+package com.emeraldscrolls.emeraldscrollsapp
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.emeraldscrolls.emeraldscrollsapp.home.RootHome
+import com.emeraldscrolls.emeraldscrollsapp.note.RootNote
+import com.emeraldscrolls.emeraldscrollsapp.ui.theme.EmeraldScrollsTheme
+
+@Composable
+fun RootApp() {
+    val navController = rememberNavController()
+    val viewModel = MainViewModel()
+    val state by viewModel.state.collectAsState()
+
+    fun onCheckClick() {
+        navController.popBackStack()
+        viewModel.saveNewNote()
+    }
+
+    fun navigateTo(route: String){
+        navController.navigate(route)
+    }
+
+    EmeraldScrollsTheme {
+        NavHost(navController = navController, startDestination = "home") {
+            composable("home") {
+                RootHome(state.notes, ::navigateTo)
+            }
+            composable("note") {
+                RootNote(viewModel::onSaveNote, ::onCheckClick)
+            }
+        }
+    }
+}
