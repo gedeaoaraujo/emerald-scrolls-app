488ac81f8143027552d0e59cbc3782d5bd2d4f86
gedeoaraujo <gedeao.lemobs@gmail.com>
2025-01-07 20:10:23 -0300

    Adicionando id à entidade.

--- app/src/main/java/com/emeraldscrolls/emeraldscrollsapp/data/entities/ScrollEntity.kt
+++ app/src/main/java/com/emeraldscrolls/emeraldscrollsapp/data/entities/ScrollEntity.kt
@@ -1,9 +1,12 @@
 package com.emeraldscrolls.emeraldscrollsapp.data.entities
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "scrolls")
 data class ScrollEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
     val title: String,
     val text: String,
     val date: String,
