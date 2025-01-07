93be3c583faeebc28c35ec33a9d7a2b6dc55fcf4
gedeoaraujo <gedeao.lemobs@gmail.com>
2025-01-07 14:03:09 -0300

    Modificando ícone de adição de nova nota.

--- app/src/main/java/com/emeraldscrolls/emeraldscrollsapp/MainActivity.kt
+++ app/src/main/java/com/emeraldscrolls/emeraldscrollsapp/MainActivity.kt
@@ -10,8 +10,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Create
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
@@ -85,7 +85,7 @@ fun RootApp() {
                 containerColor = MaterialTheme.colorScheme.primary
             ) {
                 Icon(
-                    imageVector = Icons.Default.Add,
+                    imageVector = Icons.Filled.Create,
                     contentDescription = "Add new note"
                 )
             }
