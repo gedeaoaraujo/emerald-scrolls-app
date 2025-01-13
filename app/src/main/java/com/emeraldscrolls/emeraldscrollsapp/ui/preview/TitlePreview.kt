dc313cc2abfa5f9ed90f609f6005f1224f3c3f40
gedeoaraujo <gedeao.lemobs@gmail.com>
2025-01-13 20:38:07 -0300

    Adicionando botão para imprimir pdf.

--- app/src/main/java/com/emeraldscrolls/emeraldscrollsapp/ui/preview/TitlePreview.kt
+++ app/src/main/java/com/emeraldscrolls/emeraldscrollsapp/ui/preview/TitlePreview.kt
@@ -1,6 +1,7 @@
 package com.emeraldscrolls.emeraldscrollsapp.ui.preview
 
 import android.content.Intent
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -17,8 +18,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.emeraldscrolls.emeraldscrollsapp.R
 import com.emeraldscrolls.emeraldscrollsapp.model.ScrollModel
+import com.emeraldscrolls.emeraldscrollsapp.utils.PdfCreator
 import com.emeraldscrolls.emeraldscrollsapp.utils.dateTimeFmt
 
 @Composable
@@ -45,10 +49,19 @@ fun TitlePreview(
         context.startActivity(startIntent)
     }
 
+    fun onGenerateFile(){
+        val isGenerated = PdfCreator.generatePDF(context, scroll)
+        val text = if (isGenerated) "Pdf file is generated"
+        else "Pdf file could not be generated"
+        Toast.makeText(context, text, Toast.LENGTH_LONG).show()
+    }
+
     Row(
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.SpaceBetween,
-        modifier = Modifier.fillMaxWidth().padding(horizontal = 8.dp)
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 8.dp)
     ) {
         Text(
             text = "Preview Scroll",
@@ -56,6 +69,14 @@ fun TitlePreview(
             style = MaterialTheme.typography.titleMedium
         )
         Row {
+            IconButton(onClick = {
+                scroll?.let { onGenerateFile() }
+            }) {
+                Icon(
+                    painter = painterResource(id = R.drawable.icon_pdf),
+                    contentDescription = "Print Scroll"
+                )
+            }
             IconButton(onClick = {
                 scroll?.let { onShareScroll(it) }
             }) {
